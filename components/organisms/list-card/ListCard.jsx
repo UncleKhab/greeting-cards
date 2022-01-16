6e2273f6d04e9e4cc6631f25ac9ac18f785b6006
Stefan <cobrosoffice@gmail.com>
2022-01-16 22:51:37 +0200

    fixed class issue

--- components/organisms/list-card/ListCard.jsx
+++ components/organisms/list-card/ListCard.jsx
@@ -5,7 +5,7 @@ const ListCard = ({imgSource}) => {
     return (
         <div className='flex flex-col justify-center items-center z-10'>
             <div className='w-fit z-20'>
-                <img src={imgSource} alt="img-greeting-cover" class="thumbnail"/>
+                <img src={imgSource} alt="img-greeting-cover"/>
             </div>
             <div className='bg-white p-4 pt-10 z-10 relative -top-10  m-2'>
                 <h3 className='text-xs font-bold text-slate-800'>Happy Valentine's Day</h3>
